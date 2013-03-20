6fc7be6513a623ebdde2683e84dd283299869dfa
Michael Toews <vegitalander@gmail.com>
2013-03-20 00:47:45 -0700

    Refine exercise 7 to include pluralization.

--- app/assets/javascripts/character_countdown.js
+++ app/assets/javascripts/character_countdown.js
@@ -1,7 +1,16 @@
 function updateCountdown() {
   // 140 characters max
-  var remaining = 140 - jQuery('.micropost_text_area').val().length;
-  jQuery('.countdown').text(remaining + ' characters remaining.');
+  var left = 140 - jQuery('.micropost_text_area').val().length;
+  if(left == 1) {
+    var charactersLeft = ' character left.'
+  }
+  else if(left < 0){
+    var charactersLeft = ' characters too many.'
+  }
+  else{
+    var charactersLeft = ' characters left.'
+  }
+  jQuery('.countdown').text(Math.abs(left) + charactersLeft);
 }
 
 jQuery(document).ready(function($) {
