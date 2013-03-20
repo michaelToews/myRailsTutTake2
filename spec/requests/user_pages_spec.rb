0c00aaa43d33c8585dc0bc7dba137a33553317e8
Michael Toews <vegitalander@gmail.com>
2013-03-20 16:48:20 -0700

    Change Gemfile to specify rails 3.2.13 and finish exercise 4.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -75,19 +75,38 @@ describe "UserPages" do
 
   describe "profile page" do
     describe "when the user is signed in" do
-    	let(:user) { FactoryGirl.create(:user) }
-      let!(:m1)  { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-      let!(:m2)  { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    	let(:user)       { FactoryGirl.create(:user) }
+      let!(:m1)        { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+      let!(:m2)        { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-    	before { visit user_path(user) }
+    	before do
+        sign_in user
+        visit user_path(user)
+      end
 
     	it { should have_selector('h1',    text: user.name) }
     	it { should have_selector('title', text: user.name) }
 
       describe "microposts" do
-        it { should have_content(m1.content) }
-        it { should have_content(m2.content) }
-        it { should have_content(user.microposts.count) }
+        describe "belonging to the signed in user" do
+          it { should have_content(m1.content) }
+          it { should have_content(m2.content) }
+          it { should have_content(user.microposts.count) }
+          it { should have_link("delete") }
+        end
+
+        describe "not belonging to the signed in user" do
+          let(:other_user) { FactoryGirl.create(:user) }
+          let!(:m3)        { FactoryGirl.create(:micropost, user: other_user, content: "other_post") }
+          let!(:m4)        { FactoryGirl.create(:micropost, user: other_user, content: "second other_post") }
+
+          before do
+            sign_in other_user
+            visit user_path(user)
+          end
+
+          it { should_not have_link("delete") }
+        end
       end
     end
   end
